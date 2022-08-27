3cf49528160bd846456c6aae89d1c63bde977aed
drramaqueen <tatyana.kuzina11@gmail.com>
2022-08-27 16:17:12 +0400

    fix test

--- src/bitly.rs
+++ src/bitly.rs
@@ -104,9 +104,8 @@ mod tests {
     #[tokio::test(flavor = "multi_thread")]
     async fn test_bitly_expand_without_scheme() {
         let bitly: Shortener = Default::default();
-        println!("{:?}", bitly.expand(EXPANDED).await.unwrap());
-        // let bitly = bitly.expand(EXPANDED).await.unwrap();
-        // assert_eq!(bitly.as_str(), SHORTEN);
+        let bitly = bitly.expand(EXPANDED).await.unwrap();
+        assert_eq!(bitly.as_str(), SHORTEN);
     }
 
     #[tokio::test(flavor = "multi_thread")]
